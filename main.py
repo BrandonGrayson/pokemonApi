12b24b5f1fcc12fbb98e9bbb47f378ffbee06077
Brandon Grayson <brandonbgrayson@gmail.com>
2024-01-30 21:54:28 -0500

    cors middleware

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Union
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 from fastapi import FastAPI
 
@@ -8,6 +9,18 @@ class User(BaseModel):
     password: str
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/createUser")
